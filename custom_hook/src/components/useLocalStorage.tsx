200a4b04c65713962233fc1df735d60525e025b4
Valerio Pasqua <valeriopasquapro@gmail.com>
2025-04-24 10:50:13 +0200

    pushing the map
    esercizio 2-3 aggiornato

--- custom_hook/src/components/useLocalStorage.tsx
+++ custom_hook/src/components/useLocalStorage.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 function useLocalStorage<T>(key: string, initialValue: T) {
-  const [storedValue, setStoredValue] = useState<T>(() => {
+  const [storedValue, setStoredValue] = useState(() => {
     try {
       const item = localStorage.getItem(key);
       return item ? (JSON.parse(item) as T) : initialValue;
